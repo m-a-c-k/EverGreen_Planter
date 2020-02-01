bda4b1e95de1c400a37b629675435f8e09b94e51
mack <mackstarnes@gmail.com>
2020-01-31 18:08:55 -0600

    Add files via upload

---
+++ blob_detect.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+# Standard imports
+import cv2
+import numpy as np;
+
+# Read image
+im = cv2.imread("blob.jpg", cv2.IMREAD_GRAYSCALE)
+cap = cv2.VideoCapture(0)
+
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 10
+params.maxThreshold = 200
+
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 1500
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.1
+
+# Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.87
+    
+# Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+
+# Create a detector with the parameters
+ver = (cv2.__version__).split('.')
+if int(ver[0]) < 3 :
+	detector = cv2.SimpleBlobDetector(params)
+else : 
+	detector = cv2.SimpleBlobDetector_create(params)
+
+while(True):
+# Detect blobs.
+	#keypoints = detector.detect(im)
+	ret, frame = cap.read()
+	keypoints = detector.detect(frame)
+
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
+# the size of the circle corresponds to the size of blob
+
+	#im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+	cap_w_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Show blobs
+	#cv2.imshow("Keypoints", im_with_keypoints)
+	cv2.imshow('frame',cap_w_keypoints)
+	#cv2.waitKey(0)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+	
+cap.release()
+cv2.destroyAllWindows()
