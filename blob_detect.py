5b8237f6c2384c332b9379431cb69ed60c9cea4a
m-a-c-k <mackstarnes@gmail.com>
2020-02-11 21:10:40 -0600

    tuning

--- blob_detect.py
+++ blob_detect.py
@@ -2,12 +2,12 @@
 
 # Standard imports
 import cv2
-import numpy as np;
+import numpy as np
 
 # Read image
 im = cv2.imread("blob.jpg", cv2.IMREAD_GRAYSCALE)
 cap = cv2.VideoCapture(0)
-
+i = 0
 
 # Setup SimpleBlobDetector parameters.
 params = cv2.SimpleBlobDetector_Params()
@@ -19,11 +19,11 @@ params.maxThreshold = 200
 
 # Filter by Area.
 params.filterByArea = True
-params.minArea = 1500
+params.minArea = 500
 
 # Filter by Circularity
 params.filterByCircularity = True
-params.minCircularity = 0.1
+params.minCircularity = 0.2
 
 # Filter by Convexity
 params.filterByConvexity = True
@@ -52,11 +52,17 @@ while(True):
 
 	#im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 	cap_w_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
+	img = np.zeros((500,500,3),dtype = 'uint8') # Create a dummy image
+	
 # Show blobs
 	#cv2.imshow("Keypoints", im_with_keypoints)
 	cv2.imshow('frame',cap_w_keypoints)
 	#cv2.waitKey(0)
+	cv2.imshow('a',img)
+	font = cv2.FONT_HERSHEY_SIMPLEX
+	cv2.putText(img,chr(1),(140+i,250), font, .5,(255,255,255),2,cv2.LINE_AA)
+	i+=10
+	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 	
